0eb6070d5619b4569cc3b6cd9455841a3f2c0eff
Stephen Robinson <s.j.robinson1@lancaster.ac.uk>
2021-04-27 17:42:30 +0100

    Add initial json patch action

--- index.js
+++ index.js
@@ -1,18 +1,43 @@
 const core = require('@actions/core');
-const wait = require('./wait');
-
+const yaml = require('js-yaml');
+const { applyOperation } = require('fast-json-patch');
+const fs = require('fs');
 
 // most @actions toolkit packages have async methods
 async function run() {
   try {
-    const ms = core.getInput('milliseconds');
-    core.info(`Waiting ${ms} milliseconds ...`);
+    const file = core.getInput('file');
+    const op = core.getInput('op');
+    const path = core.getInput('path');
+    const value = core.getInput('value');
+    const outputFormat = core.getInput('output')
+    
+    let fileContents = fs.readFileSync(file, 'utf-8');
+    let config = yaml.load(fileContents);
 
-    core.debug((new Date()).toTimeString()); // debug is only output if you set the secret `ACTIONS_RUNNER_DEBUG` to true
-    await wait(parseInt(ms));
-    core.info((new Date()).toTimeString());
+    let operation = {
+      op: op,
+      path: path,
+      value: value
+    }
 
-    core.setOutput('time', new Date().toTimeString());
+    let document = applyOperation(config, operation).newDocument;
+
+    switch (outputFormat) {
+      case 'yaml':
+        let yamlStr = yaml.dump(document);
+        fs.writeFileSync(file, yamlStr, 'utf-8');
+        break;
+      case 'json':
+        let data = JSON.stringify(document, null, 2)
+        fs.writeFileSync(file, data, 'utf-8')
+        break;
+      default:
+        break;
+    }
+
+    
+
   } catch (error) {
     core.setFailed(error.message);
   }
